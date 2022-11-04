270f7bcc8f79835c4786c09eead94fc8ba5a6a0f
limoiie <limo.iie4@gmail.com>
2022-11-04 15:57:58 +0800

    test: Add test cases for de_ext and file_ext

--- src/io/de_ext.rs
+++ src/io/de_ext.rs
@@ -29,3 +29,38 @@ impl<R: std::io::Read> DeExt for R {
         serde_yaml::from_reader(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_de_json() {
+        let json_string = r###"{name: limo, type: real}"###;
+        let m: HashMap<String, String> = json_string.as_bytes().de_json().unwrap();
+
+        assert_eq!(
+            m,
+            HashMap::from([
+                ("name".into(), "fake".into()),
+                ("type".into(), "real".into())
+            ])
+        )
+    }
+
+    #[test]
+    fn test_de_yaml() {
+        let yaml_string = r###"name: fake
+type: real"###;
+        let m: HashMap<String, String> = yaml_string.as_bytes().de_yaml().unwrap();
+
+        assert_eq!(
+            m,
+            HashMap::from([
+                ("name".into(), "fake".into()),
+                ("type".into(), "real".into())
+            ])
+        )
+    }
+}
--- src/path/file_ext.rs
+++ src/path/file_ext.rs
@@ -25,3 +25,44 @@ impl<P: AsRef<Path>> FileExt for P {
         std::fs::remove_file(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{Read, Write};
+
+    #[test]
+    fn test_path_buf() {
+        let ntf = tempfile::NamedTempFile::new().unwrap();
+        test_path(ntf.path().to_path_buf());
+    }
+
+    #[test]
+    fn test_path_ref() {
+        let ntf = tempfile::NamedTempFile::new().unwrap();
+        test_path(ntf.path());
+    }
+
+    #[test]
+    fn test_path_str() {
+        let ntf = tempfile::NamedTempFile::new().unwrap();
+        test_path(ntf.path().to_str().unwrap())
+    }
+
+    fn test_path<P: AsRef<Path>>(p: P) {
+        {
+            let mut f = p.create().unwrap();
+            f.write(&[10, 20]).unwrap();
+        }
+
+        assert!(p.as_ref().exists());
+        {
+            let mut f = p.open().unwrap();
+            let mut buf = [];
+            f.read(&mut buf).unwrap();
+        }
+
+        p.remove().unwrap();
+        assert!(!p.as_ref().exists());
+    }
+}
