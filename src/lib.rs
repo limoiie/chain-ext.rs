bedfd8ec62083165d29816ef8ccd4138a5aa00f9
limoiie <limo.iie4@gmail.com>
2022-11-07 10:42:17 +0800

    feat: Support apply for all Sized

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(const_trait_impl)]
+
+use std::marker::Destruct;
+
 pub mod io;
 pub mod option;
 pub mod path;
@@ -7,3 +11,47 @@ pub mod mongodb;
 
 #[cfg(feature = "mongodb-gridfs")]
 pub mod mongodb_gridfs;
+
+pub trait Apply {
+    fn apply<F, R>(self, f: F) -> R
+    where
+        F: ~const FnOnce(Self) -> R,
+        F: ~const Destruct,
+        Self: Sized;
+}
+
+impl<T: Sized> Apply for T {
+    fn apply<F, R>(self, f: F) -> R
+    where
+        F: ~const FnOnce(Self) -> R,
+        F: ~const Destruct,
+    {
+        f(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_apply_move() {
+        let v = 10;
+        let n = v.apply(|n| n + 10);
+        assert_eq!(n, 10 + 10);
+    }
+
+    #[test]
+    fn test_apply_ref() {
+        let v = 10;
+        let n = (&v).apply(|n| n + 20);
+        assert_eq!(n, 10 + 20);
+    }
+
+    #[test]
+    fn test_apply_mut_ref() {
+        let mut v = 10;
+        (&mut v).apply(|v| *v = *v + 10);
+        assert_eq!(v, 10 + 10);
+    }
+}
